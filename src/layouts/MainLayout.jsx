58013f8b7abad918b8c71ac911344c335d05f6ad
Adrian Jade Manaday <adrianjademanaday@gmail.com>
2024-10-03 22:04:11 +0800

    commiting the files

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
+const MainLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <ToastContainer />
+    </>
+  )
+}
+
+export default MainLayout
